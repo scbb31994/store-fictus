c17f2f005dd8422ba68f36ff045bd614ae27eec5
scbb31994 <steve.cabrera@brandingbrand.com>
2023-04-07 00:05:54 -0700

    Change product.model from class to interface

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,4 +1,4 @@
-export class Product {
+export interface Product {
   id: number;
   title: string;
   price: number;
@@ -6,22 +6,4 @@ export class Product {
   category: string;
   image: string;
   rating: {rate: number, count: number};
-
-  constructor(
-    id: number,
-    title: string,
-    price: number,
-    description: string,
-    category: string,
-    image: string,
-    rating: { rate: number, count: number }
-  ) {
-    this.id = id;
-    this.title = title;
-    this.price = price;
-    this.description = description;
-    this.category = category;
-    this.image = image;
-    this.rating = rating;
-  }
 }
