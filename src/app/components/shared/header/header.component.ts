4a49c6b06f3f07831c25d81f40c2e82a6ec6dabf
scbb31994 <steve.cabrera@brandingbrand.com>
2023-04-13 15:53:04 -0700

    fix cart-service import statement

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CartService } from 'src/app/services/cart.service';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-header',
