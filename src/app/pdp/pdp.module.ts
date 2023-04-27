7b3f2d269b46a29c667449896f60dc4abd3b5b42
scbb31994 <steve.cabrera@brandingbrand.com>
2023-04-26 21:51:15 -0700

    remove old folders + fix spacing on qty stepper

--- src/app/pdp/pdp.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { PdpComponent } from "./pdp.component";
-
-@NgModule({
-  declarations: [PdpComponent],
-  imports: [CommonModule]
-})
-export class PdpModule {}
